8b8bc497b8bcc57781bbd9e978090887f352fdf2
nsc-nis <niklas.schachl@htl-wels.at>
2022-11-23 10:06:54 +0100

    Styling improvements

--- src/components/ToDoList.vue
+++ src/components/ToDoList.vue
@@ -39,31 +39,55 @@ export default {
 <style scoped>
 ul {
   list-style: none;
+  flex-direction: column;
+  width: 25%;
+  margin: auto;
+  font-family: 'Lato', sans-serif;
+  font-size: 20px;
+  font-weight: 400;
+  font-style: italic;
 }
-
 ul li {
-  margin: 5px;
-  cursor: pointer;
+  margin-bottom: 10px;
+  color: #5c5c5c;
+  display: flex;
+  margin: 5px 0;
+  justify-content: space-between;
+  align-content: center;
 }
-
 ul li :hover {
   opacity: 0.4;
 }
-
 .delete {
   background-color: red;
   color: white;
-  padding-top: 2px;
-  padding-bottom: 2px;
-  /*padding: 10 0 10px;*/
-  padding-left: 10px;
-  padding-right: 10px;
+  padding: 0 10px;
   line-height: 1;
   border-radius: 5px;
   cursor: pointer;
 }
+.hideDelete {
+  opacity: 0;
+}
 
 .done {
   text-decoration: line-through;
+}
+.taskInfo {
+  margin-top: 15px;
+  font-size: 14px;
+  display: flex;
+  margin-left: auto;
+  color: #335c62;
+  justify-self: space-between;
+}
+.doneTasks {
+  position: absolute;
+  left: 20px;
+}
+input[type='checkbox'] {
+  width: 20px;
+  background: #555;
+  border-radius: 5px;
 }
 </style>
